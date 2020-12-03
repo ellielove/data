98564df5b6740f86cd83729a26f04fe7be0a1b5a
ellie <ellie@pacifist.studio>
2020-12-02 21:30:02 -0800

    1. Added saving/loading of application dict, to json files
     2. added some return types to methods
    
     3. some reordering of program loop
    
     4. added shutdown sequence method

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import os
 
 # tkinter
 import PySimpleGUI as psg
@@ -8,29 +10,49 @@ import simple_data_entry as sde
 class Application:
 
     @staticmethod
-    def create_primary_window_layout():
-        # window layout
+    def create_primary_window_layout() -> list:
+        """this method holds the specification of the layout of the primary window"""
         return [
-              [psg.Text('Browse:')]
+            [psg.Text('Browse:')]
             , [psg.Output(size=(50, 10), key='-OUTPUT-')]
             , [psg.Button('New Entry')]
         ]
+
+    @staticmethod
+    def read_project_file(path) -> dict:
+        """returns empty dict on failure, returns populated dict on successful read"""
+        if os.path.isfile(path):
+            with open(path, 'r') as infile:
+                return json.load(infile)
+        return {}
+
+    @staticmethod
+    def save_project_file(data, path) -> bool:
+        """Returns false on failure, true otherwise"""
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile, indent=4, sort_keys=True)
+        return os.path.isfile(path)
 
     def __init__(self):
         """init application, but not open window"""
         self.dict = {}
         self.window_title = 'Thought Fragment Library'
-        psg.theme('DarkAmber')
+        self.save_file_path = 'fragments.project.json'
+        self.theme = 'Reddit'
+
+        psg.theme(self.theme)
         self.layout = self.create_primary_window_layout()
         self.window = psg.Window(self.window_title, self.layout)
 
-    def modify_dictionary_entry(self, entry):
+        self.dict = self.read_project_file(self.save_file_path)
+
+    def modify_dictionary_entry(self, entry) -> None:
         """inserts a new entry, or modifies an existing entry, in the program dict """
         if entry['devname'] == '':
             return
         self.dict[entry['devname']] = entry
 
-    def run(self):
+    def run(self) -> None:
         """runs a single cycle of the application"""
 
         def refresh_output():
@@ -40,21 +62,33 @@ class Application:
                 update_str += key + ' : ' + self.dict[key]['tags'] + '\n'
             self.window['-OUTPUT-'].update(update_str)
 
+        def shutdown_sequence():
+            """performs the application shutdown sequence"""
+            self.save_project_file(self.dict, self.save_file_path)
+            self.window.close()
+
+        # activate window with an initial read
+        self.window.read(timeout=45)
+
+        # use the data we've loaded in so far
+        refresh_output()
+
         while True:
             event, values = self.window.read(timeout=150)
-            if event == psg.WINDOW_CLOSED:
+
+            # this one is going to happen the most
+            if event == '__TIMEOUT__':
+                continue
+            # this one is the most important
+            elif event in (psg.WIN_CLOSED, 'Quit'):
+                psg.Print(self.dict, do_not_reroute_stdout=False)
                 break
-            if event == 'New Entry':
+            elif event == 'New Entry':
                 data = sde.simple_data_entry_window_cycle()
                 self.modify_dictionary_entry(data)
                 refresh_output()
 
-            if event and event != '__TIMEOUT__':
-                continue
-
-        print('loop exit')
-        self.window.close()
-        print('window close')
+        shutdown_sequence()
 
 
 if __name__ == '__main__':
