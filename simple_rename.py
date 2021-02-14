975b96c91229b310fb186ea7e65aa73308324cc0
paciFIST-Studios <ellie@pacifist.studio>
2021-02-14 02:28:53 -0800

    Update simple_rename.py
    1. When using rename, the input text box is pre-filled with the old name.
    
    2. However, polling for the escape keep doesn't seem to be working

--- simple_rename.py
+++ simple_rename.py
@@ -1,23 +1,23 @@
 import PySimpleGUI as psg
 
 
-def create_layout(file_name):
+def create_layout(entry_name):
     return [
-          [psg.Text('Rename: {}'.format(file_name), key='_FILENAME_')]
-        , [psg.InputText(key='_INPUT_')]
+          [psg.Text('Rename: {}'.format(entry_name), key='_FILENAME_')]
+        , [psg.InputText(default_text=entry_name, key='_INPUT_')]
         , [psg.Button('Submit'), psg.Button('Cancel')]
     ]
 
 
 def simple_rename_window_cycle(entry):
-    """Instantiates, uses, and destorys a simple data entry window, and returns the new name"""
+    """Instantiates, uses, and destroys a simple data entry window, and returns the new name"""
     layout = create_layout(entry['devname'])
     window = psg.Window('Rename Entry', layout)
 
     while True:
         event, values = window.read(150)
 
-        if event in ('Cancel', psg.WINDOW_CLOSED):
+        if event in (psg.WINDOW_CLOSED, 'Cancel', 'Escape:27'):
             window.close()
             return {'user_quit': True}
 
