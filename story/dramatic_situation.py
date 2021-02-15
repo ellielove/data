3c4565e409def6f7d7e8056804dbf8ebd998c6cf
paciFIST-Studios <ellie@pacifist.studio>
2021-02-14 17:13:44 -0800

    Update dramatic_situation.py
    added the ability to spawn circles on the graph, and drag them around

--- story/dramatic_situation.py
+++ story/dramatic_situation.py
@@ -44,6 +44,19 @@ def create_layout():
     ]
 
 
+class DragManager:
+    def __init__(self):
+        self.start_pos = None
+        self.end_pos = None
+        self.object = None
+        self.is_active = False
+
+
+class SpawnManager:
+    def __init__(self):
+        self.is_allowed = True
+
+
 def graph_database_window_cycle():
     window = psg.Window(
         title='Graph database'
@@ -55,13 +68,58 @@ def graph_database_window_cycle():
     window.read(timeout=45)
 
     graph = window['_GRAPH_']
-    objects = {'circle': graph.DrawCircle((50, 50), 25, fill_color='white', line_color='black')}
+    drag = DragManager()
+    spawn = SpawnManager()
+    last_mouse_pos = (0, 0)
+
+    def zero_mouse_positions():
+        graph.ClickPosition = (None, None)
+        drag.start_pos = None
+        drag.end_pos = None
+        drag.is_active = False
+        drag.object = None
+        spawn.is_allowed = True
+
+    def draw_circle_at_location(x, y, r=10, fc='white', lc='black', lw=1) -> int:
+        if not spawn.is_allowed:
+            return -1
+        circle = graph.draw_circle(center_location=(x, y), radius=r, fill_color=fc, line_color=lc, line_width=lw)
+        drag.object = circle
+        spawn.is_allowed = False
+        return circle
 
     while True:
         event, values = window.read(timeout=150)
 
         if event in (psg.WINDOW_CLOSED, 'Cancel', 'Escape:27'):
             break
+
+        if '__TIMEOUT__' in event:
+            continue
+
+        if None not in graph.ClickPosition:
+            print(f'click_position={graph.ClickPosition}')
+
+        if event.startswith('_GRAPH_'):
+            x, y = values['_GRAPH_']
+
+            if event.endswith('+UP'):
+                zero_mouse_positions()
+                continue
+
+            # current mouse movement, including an unmarked mouse down
+            figures = graph.get_figures_at_location(location=(x, y))
+            if len(figures) > 0:
+                drag.object = figures[0]
+
+            if drag.object:
+                graph.relocate_figure(drag.object, x, y)
+                graph.update()
+            else:
+                draw_circle_at_location(x, y, 10)
+
+        else:
+            print('unhandled event: ', event, values)
 
     window.close()
 
